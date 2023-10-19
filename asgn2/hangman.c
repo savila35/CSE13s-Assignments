e68090864cf8190366f2b4ed28378df5619aa347
Sebastian Avila <snavila@ucsc.edu>
2023-10-19 23:23:36 +0000

    maybe fixed

--- asgn2/hangman.c
+++ asgn2/hangman.c
@@ -1,78 +1,79 @@
-#include <stdio.h>
 #include "hangman_helpers.h"
 
+#include <stdio.h>
+
 int main(int argc, char **argv) {
-	
-	// check for correct arguments
-	if (argc != 2) {
-		printf("wrong number of arguments\n");
-        	printf("usage: %s <secret word or phrase>\n", argv[0]);
-        	printf("if the secret is multiple words, you must quote it\n");
-        	exit(1);
-    	}
-	
-	validate_secret(argv[1]);		// validate phrase
 
-	unsigned long len = strlen(argv[1]);	// length of secret phrase
-	
-	int guessed[len];			// list of bools: true if letter at index guessed
-	for (unsigned long i = 0; i < len; i++) {
-		if (string_contains_character(punctuation,argv[1][i])){
-			guessed[i] = 1;
-		} else {
-			guessed[i] = 0;
-		}
-	}
+    // check for correct arguments
+    if (argc != 2) {
+        printf("wrong number of arguments\n");
+        printf("usage: %s <secret word or phrase>\n", argv[0]);
+        printf("if the secret is multiple words, you must quote it\n");
+        exit(1);
+    }
 
-	int mistakes = 0;			// mistake counter
+   if (!validate_secret(argv[1])){
+	   return 0;
+   }
 
-	int eliminated[26] = {0};		// list of bools w/ len of alphabet: true if letter eliminated
-	int elim_index = 0;
+    unsigned long len = strlen(argv[1]); // length of secret phrase
 
-	char guess = ' ';
-	int valid_guess = 0;
-	
-	// game loop
-	while(mistakes < LOSING_MISTAKE && !all_letters_guessed(guessed, len)){
-		
-		print_screen(mistakes, guessed, len, argv[1], eliminated);
+    int guessed[len]; // list of bools: true if letter at index guessed
+    for (unsigned long i = 0; i < len; i++) {
+        if (string_contains_character(punctuation, argv[1][i])) {
+            guessed[i] = 1;
+        } else {
+            guessed[i] = 0;
+        }
+    }
 
-		do {
-			guess = read_letter();
-			if (!string_contains_character(argv[1],guess)){
-				elim_index = guess - 97;
-				if (eliminated[elim_index]) {
-					valid_guess = 0;
-					continue;
-				} else {
-					eliminated[elim_index] = 1;
-					mistakes++;
-					valid_guess = 1;
-				}
-			} else {
-				valid_guess = 1;
-				for (unsigned long i = 0; i < len; i++) {
-					if (guess == argv[1][i]) {
-						if (guessed[i] == 1) {
-							valid_guess = 0;
-							continue;
-						} else {
-							guessed[i] = 1;
-						}
-					}
-				}
-			}
-		} while (!valid_guess);
+    int mistakes = 0; // mistake counter
 
+    int eliminated[26] = { 0 }; // list of bools w/ len of alphabet: true if letter eliminated
+    int elim_index = 0;
 
-	}
-	if (mistakes >= LOSING_MISTAKE){
-		print_screen(mistakes, guessed, len, argv[1], eliminated);
-		printf("You lose! The secret phrase was: %s\n", argv[1]);
-	} else {
-		print_screen(mistakes, guessed, len, argv[1], eliminated);
-		printf("You win! The secret phrase was: %s\n", argv[1]);
-	}
+    char guess = ' ';
+    int valid_guess = 0;
 
-	return 0;
+    // game loop
+    while (mistakes < LOSING_MISTAKE && !all_letters_guessed(guessed, len)) {
+
+        print_screen(mistakes, guessed, len, argv[1], eliminated);
+
+        do {
+            guess = read_letter();
+            if (!string_contains_character(argv[1], guess)) {
+                elim_index = guess - 97;
+                if (eliminated[elim_index]) {
+                    valid_guess = 0;
+                    continue;
+                } else {
+                    eliminated[elim_index] = 1;
+                    mistakes++;
+                    valid_guess = 1;
+                }
+            } else {
+                valid_guess = 1;
+                for (unsigned long i = 0; i < len; i++) {
+                    if (guess == argv[1][i]) {
+                        if (guessed[i] == 1) {
+                            valid_guess = 0;
+                            continue;
+                        } else {
+                            guessed[i] = 1;
+                        }
+                    }
+                }
+            }
+        } while (!valid_guess);
+    }
+    if (mistakes >= LOSING_MISTAKE) {
+        print_screen(mistakes, guessed, len, argv[1], eliminated);
+        printf("You lose! The secret phrase was: %s\n", argv[1]);
+    } else {
+        print_screen(mistakes, guessed, len, argv[1], eliminated);
+        printf("You win! The secret phrase was: %s\n", argv[1]);
+    }
+
+    return 0;
 }
--- asgn2/hangman_helpers.c
+++ asgn2/hangman_helpers.c
@@ -1,7 +1,7 @@
 #include "hangman_helpers.h"
 
 bool is_lowercase_letter(char c) {
-    if (97 < c && c < 122) {
+    if (97 <= c && c <= 122) {
         return 1;
     }
     return 0;
@@ -10,14 +10,14 @@ bool is_lowercase_letter(char c) {
 bool validate_secret(const char *secret) {
     if (strlen(secret) > MAX_LENGTH) {
         printf("the secret phrase is over 256 characters");
-        exit(1);
+	return 0;
     }
     while (*secret != '\0') {
         if ((97 > *secret || *secret > 122) && (*secret != 32 && *secret != 39 && *secret != 45)) {
             printf("invalid character: '%c'\n", *secret);
             printf("the secret phrase must contain only lowercase letters, spaces, hyphens, and "
                    "apostrophes\n");
-            exit(1);
+	    return 0;
         }
         ++secret;
     }
@@ -35,45 +35,46 @@ bool string_contains_character(const char *s, char c) {
 }
 
 char read_letter(void) {
-        char c = ' ';
-        do {
-                if (c != '\n') {
-                        printf("Guess a letter: ");
-                }
-                c = (char) getchar();
-        } while (97 > c || c > 122);
-    return c;
+    char c = ' ';
+    do {
+        if (c != '\n') {
+            printf("Guess a letter: ");
+        }
+        c = (char) getchar();
+    } while (!is_lowercase_letter(c));
+    return (char) c;
 }
 
-bool all_letters_guessed(int guessed[], unsigned long len){
-	int all_guessed = 1;
-	for (unsigned long i = 0; i < len; i++){
-		if (!guessed[i]) {
-			all_guessed = 0;	
-			return all_guessed;
-		}
-	}
-	return all_guessed;
+bool all_letters_guessed(int guessed[], unsigned long len) {
+    int all_guessed = 1;
+    for (unsigned long i = 0; i < len; i++) {
+        if (!guessed[i]) {
+            all_guessed = 0;
+            return all_guessed;
+        }
+    }
+    return all_guessed;
 }
 
-void print_screen(int mistakes, int guessed[], unsigned long len, const char *secret, int eliminated[]) {
-	printf("%s", CLEAR_SCREEN);
-	printf("%s\n\n",arts[mistakes]);
-	printf("    Phrase: ");             // print phrase
-        for (unsigned long i = 0; i < len; i++) {
-		if (guessed[i]) {
-			printf("%c",*secret);
-		} else {
-			printf("_");
-		}
-		++secret;
-	}
-	printf("\n");
-	printf("Eliminated: ");
-	for (int i = 0; i < 26; i++) {
-		if (eliminated[i] == 1) {
-			printf("%c", i + 97);
-		}
-	}
-	printf("\n\n");
+void print_screen(
+    int mistakes, int guessed[], unsigned long len, const char *secret, int eliminated[]) {
+    printf("%s", CLEAR_SCREEN);
+    printf("%s\n\n", arts[mistakes]);
+    printf("    Phrase: "); // print phrase
+    for (unsigned long i = 0; i < len; i++) {
+        if (guessed[i]) {
+            printf("%c", *secret);
+        } else {
+            printf("_");
+        }
+        ++secret;
+    }
+    printf("\n");
+    printf("Eliminated: ");
+    for (int i = 0; i < 26; i++) {
+        if (eliminated[i] == 1) {
+            printf("%c", i + 97);
+        }
+    }
+    printf("\n\n");
 }
