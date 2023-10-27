bfdd692e8028d93c78fefa17cc04da423265924c
Sebastian Avila <snavila@ucsc.edu>
2023-10-27 06:46:22 +0000

    barely any progress

--- asgn3/calc.c
+++ asgn3/calc.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <math.h>
+#include <unistd.h>
 
-int main(void){
+#include "messages.h"
+#include "stack.h"
+#include "mathlib.h"
+#include "operators.h"
+
+#define OPTIONS "hm"
+
+int main(int argc, char **argv) {
+	const unary_operator_fn *unary_operators = my_unary_operators;
+
+	int opt;
+	while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+		switch (opt) {
+			case 'h':
+				printf(USAGE,argv[0]);
+				return 0;
+				break;
+			case 'm':
+				unary_operators = libm_unary_operators;
+				break;
+			default:
+				printf(USAGE, argv[0]);
+				return 1;
+		}
+	}
+
+	
+
 	return 0;
 }
