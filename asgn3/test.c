1fd48090d3b65ec60197873b224feceed5e4789b
Sebastian Avila <snavila@ucsc.edu>
2023-10-27 06:49:55 +0000

    sin and cos working

--- asgn3/test.c
+++ asgn3/test.c
@@ -23,20 +23,21 @@ int main(void) {
 	assert(stack_push(65.0) == false);
 	
 	
-	 // testing operators
+	// testing operators
 
-	 assert(apply_binary_operator(operator_add));
-	 assert(apply_binary_operator(operator_sub));
-	 assert(apply_binary_operator(operator_mul));
-	 assert(apply_binary_operator(operator_div));
+	assert(apply_binary_operator(operator_add));
+	assert(apply_binary_operator(operator_sub));
+	assert(apply_binary_operator(operator_mul));
+	assert(apply_binary_operator(operator_div));
 
-	 // testing mathlib
+	// testing mathlib
 
-	 assert(Abs(-10) == 10);
-	 assert(Abs(3.4) == 3.4);
+	assert(Abs(-10) == 10);
+	assert(Abs(3.4) == 3.4);
 
-	 assert(Abs(Sin(2) - sin(2)) < EPSILON);
-	printf("%f",Sin(M_PI));
+	assert(Abs(Sin(2) - sin(2)) < EPSILON);
+	assert(Abs(Sin(10*M_PI) - sin(10*M_PI)) < EPSILON);
 
-	 assert(Abs(Cos(2) - cos(2)) < EPSILON);
+	assert(Abs(Cos(2) - cos(2)) < EPSILON);
+	assert(Abs(Sin(-3*M_PI) - sin(-3*M_PI)) < EPSILON);
 }
