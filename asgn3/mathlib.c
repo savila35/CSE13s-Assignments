6f11ed29f4419bb95429f6b72e0110cfdd47eebc
Sebastian Avila <snavila@ucsc.edu>
2023-10-27 02:05:02 +0000

    sin and cos working

--- asgn3/mathlib.c
+++ asgn3/mathlib.c
@@ -1,8 +1,80 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mathlib.h"
+
 double Abs(double x) {
 	if (x >= 0) {
 		return x;
 	} else {
-		double res =  -1 * (0 - x);
+		double res =  -1 * x;
 		return res;
 	}
+}
+
+double Sqrt(double x) {
+	// Check domain.
+	if (x < 0) {
+      		return nan("nan");
+   	}
+   	double old = 0.0;
+   	double new = 1.0;
+   	while (Abs(old - new) > EPSILON) {
+      		// Specifically, this is the Babylonian method--a simplification of
+      		// Newton's method possible only for Sqrt(x).
+      		old = new;
+      		new = 0.5 * (old + (x / old));
+	}
+	return new; 
+}
+
+double Sin(double x) {
+	double res = 0.0;
+	int sign = 1;
+	double term = x;
+	double pwr_mult = x * x;
+	double factorial = 1.0;
+	double prev_fact = 2.0;
+	double pwr = x;
+	int n = 1;
+	
+	while(Abs(term) >= EPSILON) {
+		res += sign * term;
+		sign = -sign;
+		factorial *= prev_fact * (2 * n + 1);
+		pwr *= pwr_mult;
+		term = pwr / factorial;
+		n++;
+		prev_fact = 2 * n;
+	}
+	return res;
+}
+
+double Cos(double x) {
+	double res = 0.0;
+	int sign = 1;
+	double term = 1;
+	double pwr = x * x;
+	double pwr_mult = x * x;
+	double factorial = 1.0;
+	double prev_fact = 1.0;
+	int n = 1;
+
+	while (Abs(term) >= EPSILON) {
+		res += sign * term;
+		sign = -sign;
+		factorial *= prev_fact *  2 * n;
+		term = pwr / factorial;
+		prev_fact = 2 * n + 1;
+		n++;
+		pwr *= pwr_mult;
+	}
+	return res;	
+}
+
+double Tan(double x) {
+	double sin = Sin(x);
+	double cos = Cos(x);
+	return sin / cos;
 }
--- asgn3/test.c
+++ asgn3/test.c
@@ -20,7 +20,7 @@ int main(void) {
         assert(stack_push(1.0 * i) == true);
 	}
 
-   	 assert(stack_push(65.0) == false);
+	assert(stack_push(65.0) == false);
 	
 	
 	 // testing operators
@@ -30,4 +30,13 @@ int main(void) {
 	 assert(apply_binary_operator(operator_mul));
 	 assert(apply_binary_operator(operator_div));
 
+	 // testing mathlib
+
+	 assert(Abs(-10) == 10);
+	 assert(Abs(3.4) == 3.4);
+
+	 assert(Abs(Sin(2) - sin(2)) < EPSILON);
+	printf("%f",Sin(M_PI));
+
+	 assert(Abs(Cos(2) - cos(2)) < EPSILON);
 }
