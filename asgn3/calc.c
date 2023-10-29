948165a5fefb673953f3ade03bb733e59337026c
Sebastian Avila <snavila@ucsc.edu>
2023-10-29 22:50:34 +0000

    maybe done

--- asgn3/calc.c
+++ asgn3/calc.c
@@ -21,33 +21,70 @@ int main(int argc, char **argv) {
             printf(USAGE, argv[0]);
             return 0;
             break;
-        case 'm': unary_operators = libm_unary_operators; break;
-        default: printf(USAGE, argv[0]); return 1;
+        case 'm': 
+	    unary_operators = libm_unary_operators; 
+	    break;
+        default: 
+	    printf(USAGE, argv[0]); 
+	    return 1;
         }
     }
-    assert(apply_unary_operator(unary_operators['s']) == false);
 
-    int loop = 0;
     while (1) { //input loop
         double x = 0;
         char input_line[STACK_CAPACITY];
         char *saveptr;
-        //bool error = false;
-        fgets(input_line, STACK_CAPACITY, stdin);
-        const char *token = strtok_r(input_line, " ", &saveptr);
-        while (token != NULL) {
-            printf("here");
-            if (parse_double(token, &x)) {
-                printf("%f", x);
-            }
+        bool error = false;
 
+	fprintf(stderr, "> ");
+        if (fgets(input_line, STACK_CAPACITY, stdin) == NULL) {
+		break;
+	}
+	for (unsigned long i =0; i < strlen(input_line);i++) {
+		if (input_line[i] == '\n') {
+			input_line[i] = '\0';
+		}
+	}
+        
+	const char *token = strtok_r(input_line, " ", &saveptr);
+        
+	while (token != NULL && !error) {
+	    if (parse_double(token, &x)) {
+		    if (!stack_push(x)) {
+			    fprintf(stderr,ERROR_NO_SPACE, x);
+			    stack_clear();
+			    error = true;
+		    }
+	    } else if (strlen(token) > 1) {
+		    fprintf(stderr, ERROR_BAD_STRING, token);
+		    error = true;
+            } else if (binary_operators[(unsigned)*token] != 0 && binary_operators[(unsigned)*token] != NULL) {
+		    if (!apply_binary_operator(binary_operators[(unsigned)*token])) {
+			    fprintf(stderr, ERROR_BINARY_OPERATOR);
+			    stack_clear();
+			    error = true;
+						    
+		    }
+	    } else if (unary_operators[(unsigned)*token] != 0 && unary_operators[(unsigned)*token] != NULL) {
+		    if (!apply_unary_operator(unary_operators[(unsigned)*token])) {
+			    fprintf(stderr, ERROR_UNARY_OPERATOR);
+			    stack_clear();
+			    error = true;
+		    }
+	    } else {
+		    fprintf(stderr, ERROR_BAD_CHAR, *token);
+		    stack_clear();
+		    error = true;
+	    }
+	
             token = strtok_r(NULL, " ", &saveptr);
         }
-        x++;
-        if (loop == 1) {
-            return 0;
-        }
-        loop++;
+	
+	stack_print();
+	stack_clear();
+	if (!error) {
+		printf("\n");
+	}
     }
 
     return 0;
