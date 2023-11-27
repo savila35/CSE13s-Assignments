fe0dbf798a5b8e5e3605f58e4738de52d672eb91
Sebastian Avila <snavila@ucsc.edu>
2023-11-27 22:58:07 +0000

    886

--- asgn7/bad_xd.c
+++ asgn7/bad_xd.c
@@ -1,7 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#define X(x) printf("%08x: ", (int) x)
 #define r(o) return o
 void p(char *b, long l) {
     for (long i = 1; i <= 16; i++) {
@@ -16,26 +15,22 @@ void p(char *b, long l) {
 int main(int c, char **v) {
     if (c > 2)
         r(1);
-    int f = c == 2 ? open(v[1], O_RDONLY, 0) : 0;
+    int f = c == 2 ? open(v[1], O_RDONLY, 0) : 0, x = 0;
     if (f == -1)
         r(1);
     char b[17] = "";
-    long r = read(f, &b, 16), x = 0, t;
+    long r = 0, t;
     while (1) {
-        if (r == 0)
-            break;
+        if ((t = read(f, &b, 16 - (size_t) r)))
+            r += t;
+        else {
+            if (r)
+                printf("%08x: ", x), p(b, r);
+            r(0);
+        }
         if (r < 0)
             r(1);
-        if (r == 16) {
-            X(x), p(b, 16), r = 0, x += 16;
-        }
-        t = read(f, b + r, 16 - (size_t) r);
-        if (t == 0)
-            break;
-        r += t;
+        if (r == 16)
+            printf("%08x: ", x), p(b, r), r = 0, x += 16;
     }
-    if (r > 0) {
-        X(x), p(b, r);
-    }
-    r(0);
 }
