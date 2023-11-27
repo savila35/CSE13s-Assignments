7f1da8865e2dc2534886aa3c6bca6f29c149693e
Sebastian Avila <snavila@ucsc.edu>
2023-11-27 23:10:59 +0000

    896

--- asgn7/bad_xd.c
+++ asgn7/bad_xd.c
@@ -4,7 +4,7 @@
 #define r(o) return o
 void p(char *b, long l) {
     for (long i = 1; i <= 16; i++) {
-        printf(i > l ? "  " : "%02x", (unsigned char) b[i - 1]), printf(i % 2 == 0 ? " " : "");
+        printf(i > l ? "  " : "%02x", (unsigned) b[i - 1]), printf(i % 2 == 0 ? " " : "");
     }
     printf(" ");
     for (long j = 0; j < l; j++) {
@@ -21,7 +21,7 @@ int main(int c, char **v) {
     char b[17] = "";
     long r = 0, t;
     while (1) {
-        if ((t = read(f, &b, 16 - (size_t) r)))
+        if ((t = read(f, b + r, 16 - (size_t) r)))
             r += t;
         else {
             if (r)
@@ -30,7 +30,8 @@ int main(int c, char **v) {
         }
         if (r < 0)
             r(1);
-        if (r == 16)
+        if (r == 16) {
             printf("%08x: ", x), p(b, r), r = 0, x += 16;
+        }
     }
 }
