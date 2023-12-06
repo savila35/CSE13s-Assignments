568c31954cbb79eee01d7b124e47889c7a79d0be
Sebastian Avila <snavila@ucsc.edu>
2023-12-06 22:15:44 +0000

    not working

--- asgn8/dehuff.c
+++ asgn8/dehuff.c
@@ -1,3 +1,104 @@
-int main(void) {
+#include "bitreader.h"
+#include "bitwriter.h"
+#include "node.h"
+#include "pq.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+#define OPTIONS "hi:o:"
+#define USAGE   "Usage: dehuff -i infile -o outfile\n       dehuff -h\n"
+
+void dehuff_decompress_file(FILE *fout, BitReader *inbuf) {
+    Node *stack[64];
+    int stack_top = -1;
+    uint8_t type1 = bit_read_uint8(inbuf);
+    uint8_t type2 = bit_read_uint8(inbuf);
+    assert(type1 == 'H');
+    assert(type2 == 'C');
+    uint32_t filesize = bit_read_uint32(inbuf);
+    uint16_t num_leaves = bit_read_uint16(inbuf);
+    int num_nodes = 2 * num_leaves - 1;
+    Node *node;
+    for (int i = 0; i < num_nodes; i++) {
+        uint8_t bit = bit_read_bit(inbuf);
+        if (bit == 1) {
+            uint8_t symbol = bit_read_uint8(inbuf);
+            node = node_create(symbol, 0);
+        } else {
+            node = node_create(0, 0);
+            node->right = stack[stack_top--];
+            node->left = stack[stack_top--];
+        }
+        printf("stack top: %d\n", stack_top);
+        stack[++stack_top] = node;
+    }
+    Node *code_tree = stack[stack_top--];
+    for (uint32_t j = 0; j < filesize; j++) {
+        node = code_tree;
+        while (1) {
+            uint8_t bit = bit_read_bit(inbuf);
+            if (bit == 0) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+            if (node->left == NULL && node->right == NULL) {
+                break;
+            }
+            fputc(node->symbol, fout);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    char *fin = NULL;
+    FILE *fout = NULL;
+
+    int opt;
+    opterr = 0;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'h': printf(USAGE); return 0;
+        case 'i':
+            fin = optarg;
+            if (fin == NULL) {
+                fprintf(stderr, "dehuff:  error reading input file %s\n", optarg);
+                fprintf(stderr, USAGE);
+                return 1;
+            }
+            break;
+        case 'o':
+            fout = fopen(optarg, "wb");
+            if (fout == NULL) {
+                fprintf(stderr, "dehuff:  error opening output file %s\n", optarg);
+                fprintf(stderr, USAGE);
+                return 1;
+            }
+            break;
+
+        default:
+            fprintf(stderr, "dehuff:  unknown or poorly formatted option -%c\n", optopt);
+            fprintf(stderr, USAGE);
+            return 1;
+        }
+    }
+
+    if (fin == NULL) {
+        fprintf(stderr, "dehuff:  -i option is required\n");
+        fprintf(stderr, USAGE);
+        return 1;
+    }
+    if (fout == NULL) {
+        fprintf(stderr, "dehuff:  -o option is required\n");
+        fprintf(stderr, USAGE);
+        return 1;
+    }
+
+    BitReader *inbuf = bit_read_open(fin);
+    dehuff_decompress_file(fout, inbuf);
+
+    fclose(fout);
+    bit_read_close(&inbuf);
     return 0;
 }
--- asgn8/huff.c
+++ asgn8/huff.c
@@ -4,6 +4,15 @@
 #include "pq.h"
 
 #include <stdio.h>
+#include <unistd.h>
+
+#define OPTIONS "hi:o:"
+#define USAGE   "Usage: huff -i infile -o outfile\n       huff -h\n"
+
+typedef struct Code {
+    uint64_t code;
+    uint8_t code_length;
+} Code;
 
 uint32_t fill_histogram(FILE *fin, uint32_t *histogram) {
     uint32_t filesize = 0;
@@ -41,8 +50,108 @@ Node *create_tree(uint32_t *histogram, uint16_t *num_leaves) {
     return dequeue(q);
 }
 
+void fill_code_table(Code *code_table, Node *node, uint64_t code, uint8_t code_length) {
+    if (node->symbol == '0') {
+        fill_code_table(code_table, node->left, code, code_length + 1);
+        code |= (uint64_t) 1 << code_length;
+        fill_code_table(code_table, node->right, code, code_length + 1);
+    } else {
+        code_table[node->symbol].code = code;
+        code_table[node->symbol].code_length = code_length;
+    }
+}
+
+void huff_write_tree(BitWriter *outbuf, Node *node) {
+    if (node->left == NULL) {
+        bit_write_bit(outbuf, 1);
+        bit_write_uint8(outbuf, node->symbol);
+    } else {
+        huff_write_tree(outbuf, node->left);
+        huff_write_tree(outbuf, node->right);
+        bit_write_bit(outbuf, 0);
+    }
+}
+
+void huff_compress_file(BitWriter *outbuf, FILE *fin, uint32_t filesize, uint16_t num_leaves,
+    Node *code_tree, Code *code_table) {
+    bit_write_uint8(outbuf, 'H');
+    bit_write_uint8(outbuf, 'C');
+    bit_write_uint32(outbuf, filesize);
+    bit_write_uint16(outbuf, num_leaves);
+    while (1) {
+        int b = fgetc(fin);
+        if (b == EOF) {
+            break;
+        }
+        uint64_t code = code_table[b].code;
+        uint8_t code_length = code_table[b].code_length;
+        for (int i = 0; i < code_length; i++) {
+            bit_write_bit(outbuf, (code & 1));
+            code >>= 1;
+        }
+    }
+    huff_write_tree(outbuf, code_tree);
+}
+
 int main(int argc, char **argv) {
-    (void) argc;
-    (void) argv;
+    FILE *fin = NULL;
+    char *fout = NULL;
+
+    int opt;
+    opterr = 0;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'h': printf(USAGE); return 0;
+        case 'i':
+            fin = fopen(optarg, "rb");
+            if (fin == NULL) {
+                fprintf(stderr, "huff:  error reading input file %s\n", optarg);
+                fprintf(stderr, USAGE);
+                return 1;
+            }
+            break;
+        case 'o':
+            fout = optarg;
+            if (fout == NULL) {
+                fprintf(stderr, "huff:  error opening output file %s\n", optarg);
+                fprintf(stderr, USAGE);
+                return 1;
+            }
+            break;
+
+        default:
+            fprintf(stderr, "huff:  unknown or poorly formatted option -%c\n", optopt);
+            fprintf(stderr, USAGE);
+            return 1;
+        }
+    }
+
+    if (fin == NULL) {
+        fprintf(stderr, "huff:  -i option is required\n");
+        fprintf(stderr, USAGE);
+        return 1;
+    }
+    if (fout == NULL) {
+        fprintf(stderr, "huff:  -o option is required\n");
+        fprintf(stderr, USAGE);
+        return 1;
+    }
+
+    uint32_t histogram[256] = { 0 };
+    uint32_t filesize = fill_histogram(fin, histogram);
+    uint16_t num_leaves = 0;
+    Node *tree = create_tree(histogram, &num_leaves);
+
+    Code code_table[256];
+    fill_code_table(code_table, tree, 0, 0);
+
+    BitWriter *outbuf = bit_write_open(fout);
+
+    fseek(fin, 0, SEEK_SET);
+    huff_compress_file(outbuf, fin, filesize, num_leaves, tree, code_table);
+
+    fclose(fin);
+    node_free(&tree);
+    bit_write_close(&outbuf);
     return 0;
 }
